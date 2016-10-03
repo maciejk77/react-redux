fc63c41ad9bafb18f6fa0725d00ae5fc81e29d5a
Maciej Kurek <maciejk77@gmail.com>
2016-10-03 20:48:44 +0100

    Boilerplate code and setting up first reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() { 
+  return [
+    {title: 'JavaScript: the Good Parts'},
+    {title: 'Angular, From Novice to Ninja'},
+    {title: 'Eloquent Ruby'},
+    {title: 'HTML for Dummies'}
+  ]
+}
